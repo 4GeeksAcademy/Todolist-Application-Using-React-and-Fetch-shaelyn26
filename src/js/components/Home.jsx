7aa313322c3de7215978359b76b9b1650f2e696a
Shaelyn26 <shaelynb0405@gmail.com>
2025-03-15 00:24:35 +0000

    successul api addition

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,60 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [choresArray, setChoresArray] = useState([])
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const deleteToDo = (itemToDelete) => {
+    const result = choresArray.filter((item) => item != itemToDelete);
+    setChoresArray(result);
+  };
+  	const toDoUrl= "https://playground.4geeks.com/todo/users/shaelyn26"
+	const getToDos = () => {
+		fetch(toDoUrl)
+		.then((resp)=> resp.json())
+		.then((data)=> setChoresArray(data.todos)) 
+	}
+	useEffect(()=> {
+		if (choresArray.length == 0){
+			getToDos()
+
+		}
+	},[])
+			
+  return (
+    <div className="container">
+      <div className="title">
+        <h1>My To-Do List</h1>
+      </div>
+      <ul>
+        {
+		choresArray.length > 0 ? 
+		choresArray.map((item) => {
+			return (
+			  <div className="list d-flex">
+				{item.label}
+				<span className="delete-btn mx-2" onClick={() => deleteToDo(item)} onMouseOver={(e) => e.target.choresArray = "hover"}>
+				  x
+				</span>
+			  </div>
+			);
+		  })
+		: "there is no todos"
+		}
+      </ul>
+      <input
+      type="text"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+          const newChoreArray = [...choresArray,e.target.value]
+            setChoresArray(newChoreArray)
+          }
+        }}
+      />
+    </div>
+  );
 };
 
 export default Home;
